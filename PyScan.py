ecf9497dcb930ad7915bbb9e5e00523cd80d0136
Vigly <robert.james.oconnor@outlook.com>
2020-05-24 16:11:03 -0700

    Added SSH, HTTP, and HTTPS Banner Grabbing

--- PyScan.py
+++ PyScan.py
@@ -2,7 +2,13 @@ import socket
 import sys
 import threading
 import time
+import requests
+import urllib3
 from queue import Queue
+
+urllib3.disable_warnings()
+
+open_ports = []
 
 #Sets socket lockout time for port status accuracy
 socket.setdefaulttimeout(0.55)
@@ -25,7 +31,7 @@ def portscan(port):
         connection = sock.connect((remote_ip, port))
 
         with print_lock:
-            print("[+]", port)
+            open_ports.append(port)
 
         connection.close()
     except:
@@ -39,8 +45,8 @@ def threader():
 
 q = Queue()
 
-#Use 100 threads
-for x in range(100):
+#Use 200 threads
+for x in range(200):
     t = threading.Thread(target = threader)
     t.daemon = True
     t.start()
@@ -50,6 +56,30 @@ for worker in range(1, 65535):
     q.put(worker)
 
 q.join()
+
+#Enumerate known open ports
+for port in open_ports:
+    if (port == 22):
+        s = socket.socket()
+        s.connect((remote_ip, port))
+        data = s.recv(1024)
+        s.close()
+        banner = data.decode('utf-8')
+        print("[+] 22:", banner.rstrip())
+        open_ports.remove(port)
+    if (port == 80):
+        url = "http://" + remote_ip
+        r = requests.head(url)
+        print("[+] 80:", r.headers['Server'])
+        open_ports.remove(port)
+    if (port == 443):
+        url = "https://" + remote_ip
+        r = requests.head(url, verify=False)
+        print("[+] 443:", r.headers['Server'])
+        open_ports.remove(port)
+
+print("\nOpen ports that need manual enumeration:")
+print(*open_ports, sep = "\n")
 
 #Gets current time after scan runs
 time2 = time.time()
